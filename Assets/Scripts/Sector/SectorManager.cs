6316f1256e83f1c0c1f1d509739298ba1e1c77e4
Janarci <86641308+Janarci@users.noreply.github.com>
2022-12-11 23:27:05 +0800

    Update SectorManager.cs

--- Assets/Scripts/Sector/SectorManager.cs
+++ Assets/Scripts/Sector/SectorManager.cs
@@ -19,7 +19,7 @@ public class SectorManager : MonoBehaviour
      */
     [SerializeField] private Dictionary<int, Sector> sectorList;
 
-    private float spawnCatInterval = 5.0f;
+    private float spawnCatInterval = 100.0f;
 
     // Start is called before the first frame update
     void Start()
