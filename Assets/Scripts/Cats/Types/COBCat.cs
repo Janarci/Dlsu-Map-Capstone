7b7ad3cf91932a868bc47bdcc0dc4cbef89f9b18
KyleDesquitado <kyle_francis_desquitado@dlsu.edu.ph>
2023-02-26 13:51:48 +0800

    cats

---
+++ Assets/Scripts/Cats/Types/COBCat.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using EvolutionMaterialInventory = System.Collections.Generic.Dictionary<CatEvolutionItem.cat_evolution_item_type, int>;
+
+
+public class COBCat : Cat
+{
+
+
+
+    private Dictionary<cat_type, EvolutionMaterialInventory> COB_cat_evolution_requirements = new Dictionary<cat_type, EvolutionMaterialInventory>()
+    {
+        
+    };
+
+    public override IDictionary<cat_type, EvolutionMaterialInventory> evolution_requirements
+    {
+        get
+        {
+            return COB_cat_evolution_requirements;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        Debug.Log(this.GetCatType());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected override void InitializeCatType()
+    {
+        school_tip = "Want to look for COB students? They usually hold their classes at St. La Salle Hall!";
+        this.type = cat_type.COB_cat;
+    }
+
+    //protected override void InitializeCatFavors()
+    //{
+    //    InitializeFoodFavors(10, 20, 5);
+    //    InitializeToyFavors(10, 5, 20);
+
+    //}
+
+    protected override void InitializeEvolutionPath()
+    {
+        //evolution_requirements = new Dictionary<cat_type, EvolutionMaterialInventory>();
+
+    }
+}
---
+++ Assets/Scripts/Cats/Types/HistoryCat.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using EvolutionMaterialInventory = System.Collections.Generic.Dictionary<CatEvolutionItem.cat_evolution_item_type, int>;
+
+
+public class HistoryCat : Cat
+{
+    private Dictionary<cat_type, EvolutionMaterialInventory> History_cat_evolution_requirements = new Dictionary<cat_type, EvolutionMaterialInventory>()
+    {
+
+    };
+
+    public override IDictionary<cat_type, EvolutionMaterialInventory> evolution_requirements
+    {
+        get
+        {
+            return History_cat_evolution_requirements;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        Debug.Log(this.GetCatType());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected override void InitializeCatType()
+    {
+        school_tip = "There’s a Filipinana Section in Learning Commons! It’s on the 9th floor.";
+        this.type = cat_type.history_cat;
+    }
+
+    //protected override void InitializeCatFavors()
+    //{
+    //    InitializeFoodFavors(10, 20, 5);
+    //    InitializeToyFavors(10, 5, 20);
+
+    //}
+
+    protected override void InitializeEvolutionPath()
+    {
+        //evolution_requirements = new Dictionary<cat_type, EvolutionMaterialInventory>();
+
+
+    }
+}
